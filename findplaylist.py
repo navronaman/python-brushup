a7616dbf277e9d88f436a957f21cb13712337675
navronaman <namanmaulikshah@gmail.com>
2024-01-02 22:33:53 -0500

    Added find playlist function

--- findplaylist.py
+++ findplaylist.py
@@ -8,9 +8,6 @@ import json
 
 load_dotenv()
 
-PLAYLIST_ID = "5KGUSMWkfWedc067zW7BYM"
-GET_PLAYLIST_URL = f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}"
-
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
@@ -30,18 +27,44 @@ def get_token():
     }
     
     # We will be sending a Post request 
-    
-    auth_options = {}
-    
+        
     result = requests.post(url=url, headers=header, data=form)
-    json_result =  json.load(result.content)
+    json_result =  json.loads(result.content)
     
     token = json_result["access_token"]
     return token
 
+
+token = get_token()
+
+def get_auth_header(token):
+    return {
+        "Authorization" : "Bearer " + token
+    }
     
-    
+header_auth = get_auth_header(token=token)
+
+PLAYLIST_ID = "5KGUSMWkfWedc067zW7BYM"
+GET_PLAYLIST_URL = f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}"
     
 
+def get_playlist(playlist_id, fields, market="US"):
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+    headers = get_auth_header(token=token)
+    
+    query = f"?fields={fields}&market={market}"
+            
+    query_url = url + query
+    
+    result = requests.get(url=query_url, headers=header_auth)
+
+    json_result = json.loads(result.content)
+    
+    return json_result
+
+if __name__ == "__main__":
+    json = get_playlist("5KGUSMWkfWedc067zW7BYM", "images")
+    print(json["images"][0]["url"])
+    
 
 
