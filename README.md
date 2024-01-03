06e673a75aca541386afa77c3a415157226b8f11
navronaman <namanmaulikshah@gmail.com>
2024-01-02 22:02:26 -0500

    Fetching tokens from spotify API

---
+++ README.md
@@ -0,0 +1,5 @@
+"""
+What is the point of this program?
+
+We have all the playlists from the user, and the song inputted is checked as to see what playlist it is in
+"""
--- findplaylist.py
+++ findplaylist.py
@@ -1,12 +1,8 @@
-"""
-What is the point of this program?
-
-We have all the playlists from the user, and the song inputted is checked as to see what playlist it is in
-"""
-
 import requests
 import os
 from dotenv import load_dotenv
+import base64
+import json
 
 # We will try to get one playlists from the user first
 
@@ -18,11 +14,34 @@ GET_PLAYLIST_URL = f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}"
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-print(CLIENT_ID, CLIENT_SECRET)
+def get_token():
+    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+    
+    url = "https://accounts.spotify.com/api/token"
+    header = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type" : "application/x-www-form-urlencoded"
+    }
+        
+    form = {
+        "grant_type": "client_credentials"
+    }
+    
+    # We will be sending a Post request 
+    
+    auth_options = {}
+    
+    result = requests.post(url=url, headers=header, data=form)
+    json_result =  json.load(result.content)
+    
+    token = json_result["access_token"]
+    return token
 
-
-
-
+    
+    
+    
 
 
 
