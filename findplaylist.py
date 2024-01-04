d42dc911010540b381f4cb633bbb25e01f48b364
navronaman <namanmaulikshah@gmail.com>
2024-01-03 21:01:00 -0500

    Implemented get user playlist

--- findplaylist.py
+++ findplaylist.py
@@ -11,7 +11,7 @@ load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-def get_token():
+def get_token_cc():
     auth_string = CLIENT_ID + ":" + CLIENT_SECRET
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
@@ -35,36 +35,54 @@ def get_token():
     return token
 
 
-token = get_token()
+token = get_token_cc()
 
-def get_auth_header(token):
+def get_auth_header_cc(token):
     return {
         "Authorization" : "Bearer " + token
     }
     
-header_auth = get_auth_header(token=token)
+header_auth = get_auth_header_cc(token=token)
 
 PLAYLIST_ID = "5KGUSMWkfWedc067zW7BYM"
 GET_PLAYLIST_URL = f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}"
-    
+
 
 def get_playlist(playlist_id, fields, market="US"):
     url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
-    headers = get_auth_header(token=token)
+    headers = get_auth_header_cc(token=token)
     
     query = f"?fields={fields}&market={market}"
             
     query_url = url + query
     
-    result = requests.get(url=query_url, headers=header_auth)
+    result = requests.get(url=query_url, headers=headers)
 
     json_result = json.loads(result.content)
     
     return json_result
 
+def get_user_playlist(limit=50, offset=10):
+    url = f"https://api.spotify.com/v1/me/playlists"
+    headers = get_auth_header_cc(token)
+    query = f"?limit={limit}&offset={offset}"
+    
+    query_url = url + query
+    
+    result = requests.get(url=query_url, headers=headers)
+    
+    json_result = json.loads(result.content)
+    
+    return json_result
+    
+
+
 if __name__ == "__main__":
-    json = get_playlist("5KGUSMWkfWedc067zW7BYM", "images")
-    print(json["images"][0]["url"])
+    json1 = get_playlist("5KGUSMWkfWedc067zW7BYM", "images")
+    print(json1["images"][0]["url"])
+    
+    json2 = get_user_playlist()
+    print(json2)
     
 
 
