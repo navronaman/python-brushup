d391c86f53f70209f4bae7ed857d84935e8ea9de
navronaman <namanmaulikshah@gmail.com>
2024-01-07 23:07:21 -0500

    Integrating OAuth2.0 Login

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 # This is the Flask app
-from flask import Flask, render_template, request, redirect
-from functions import what_playlist_what_song, create_monthly_array
-from backend import Song
-import secrets
-import requests
-from dotenv import load_dotenv
+
+# From the backend files
+from functions import what_playlist_what_song, create_monthly_array 
+from backend import Song 
+
+# Imports from flask
+from flask import Flask, render_template, request, redirect, jsonify, session
+
+# For the client ID and client Secret
+import secrets 
+from dotenv import load_dotenv 
 import os
+import base64
+
+# For the API calls within OAuth 2.0
+import requests 
 import urllib.parse
+import json
+
+# For time
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -89,6 +102,82 @@ def login():
     
     return redirect(auth_url)
 
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return jsonify({"error": request.args['error']})
+    
+    if 'code' in request.args:
+        
+        req_body = {
+            'code' : request.args['code'],
+            'grant_type' : 'authorization_code',
+            'redirect_uri' : REDIRECT_URL,
+            'client_id' : CLIENT_ID,
+            'client_secret' : CLIENT_SECRET            
+        }
+        
+        response = requests.post(url=TOKEN_URL, data=req_body)
+        token_info = json.loads(response.content)
+        
+        session["access_token"] = token_info["access_token"]
+        session["refresh_token"] = token_info["refresh_token"]
+        session["expires_at"] = datetime.now().timestamp() + token_info["expires_in"]
+        
+        return redirect('/playlists')
+    
+@app.route('/playlists')
+def get_playlists():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session["expires_at"]:
+        return redirect('/refresh-token')
+    
+    if 'access_token' in session or datetime.now().timestamp() > session["expires_at"]:
+    
+        headers = {
+            'Authorization' : f"Bearer {session['access_token']}"
+        }
+        
+        result = requests.get(url="https://api.spotify.com/v1/me/playlists", headers=headers)
+        playlists = json.loads(result.content)
+        
+        return jsonify(playlists)
+    
+    else:
+        return jsonify({"HELLO" : "I NEED HELP"})
+        
+@app.route('/refesh-token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session["expires_at"]:
+        req_body = {
+            'grant_type' : 'refresh_token',
+            'refresh_token' : session['refresh_token']
+        }
+        
+        auth_string = CLIENT_ID + ":" + CLIENT_SECRET
+        auth_bytes = auth_string.encode("utf-8")
+        auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+        
+        header = {
+            "Authorization": "Basic " + auth_base64,
+            "Content-Type" : "application/x-www-form-urlencoded"
+        }
+        
+        result = requests.post(url=AUTH_URL, headers=header, data=req_body)
+        
+        new_token_info = json.loads(result.content)
+        
+        session["access_token"] = new_token_info["access_token"]
+        session["refresh_token"] = new_token_info["refresh_token"]
+        session["expires_at"] = datetime.now().timestamp() + new_token_info["expires_in"]
+        
+        return redirect('/playlist')
+    
 
 if __name__ == "__main__":
-    app.run(host="120.0.0.1", port=0000, debug=True)
+    app.run(host="0.0.0.0", debug=True)
